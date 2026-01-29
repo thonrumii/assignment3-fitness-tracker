cfb969b77a00d0938762a8ef218e9f8d140a1490
aidad <aidadushayeva@gmail.com>
2026-01-29 16:30:30 +0500

    feat: add reflection and sorting utilities (lambdas)

---
+++ src/utils/SortUtils.java
@@ -0,0 +1,16 @@
+package utils;
+
+import model.Workout;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortUtils {
+
+    public static void sortWorkoutsByDuration(List<Workout> workouts) {
+        workouts.sort(Comparator.comparingInt(Workout::getDuration));
+    }
+
+    public static void sortWorkoutsByName(List<Workout> workouts) {
+        workouts.sort((a, b) -> a.getName().compareToIgnoreCase(b.getName()));
+    }
+}
